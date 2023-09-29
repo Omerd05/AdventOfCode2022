bfbbaeec5e2d7bdc8304b8d859e5861ccba2c084
Omer Drori <73065220+Omerd05@users.noreply.github.com>
2023-09-29 12:59:11 +0300

    Update Advent.cpp
    Solutions to day 6 and day 8 have been added.

--- Advent.cpp
+++ Advent.cpp
@@ -7,10 +7,212 @@
 #include <deque>
 #include <sstream>
 #include <stack>
+#include <fstream>
 
 
 using namespace std;
 #define print(sol) (cout << "Solution is : " << sol << '\n') 
+
+vector<int> nearestBigger(vector<int> v) { //returning the distances
+    int n = v.size();
+    for (int i = 0; i < n; i++)v[i] *= -1;
+    vector<int> ans(n,-1);
+    stack<pair<int, int>> s; s.push({ v[n - 1] ,n - 1 });
+    for (int i = n - 2; i >= 0; i--) {
+        while (s.size() && s.top().first >= v[i]) {
+            ans[s.top().second] = i + 1;
+            s.pop();
+        }
+        s.push({ v[i],i });
+    }
+    while (s.size()) {
+        int idx = s.top().second;
+        ans[idx] = 1;
+        s.pop();
+    }
+    for (int i = 0; i < n; i++)ans[i]--;
+
+    for (int i = 0; i < n; i++) {
+        ans[i] = i - ans[i];
+    }
+    return ans;
+}
+
+void day8V2() { 
+    //Main idea: for each row\col, each index is going know its nearest bigger values, to each side. Luckily, i've already solved 
+    //such question in CSES (Called nearest smaller values, using monotonic stacks).
+    //After this calculation, we'll simply travel over all indexes and will take the maximum over this product.
+
+    ifstream inputFile;
+    string line;
+    vector<vector<int>> grid;
+
+    inputFile.open("inputFile.txt");
+
+    while (getline(inputFile, line)) {
+        vector<int> row;
+        for (auto ch : line) {
+            row.push_back((int)ch - '0');
+        }
+        grid.push_back(row);
+        row.clear();
+    }
+    inputFile.close();
+    vector<vector<vector<int>>> plane(grid.size(),vector<vector<int>>(grid[0].size(),vector<int>(4,1))); //saving distance in each direction
+    int j = 0;
+    for (auto row : grid) {
+        int n = row.size();
+        auto v = nearestBigger(row);
+        for (int i = 0; i < v.size(); i++)plane[j][i][0] = v[i];
+        reverse(row.begin(),row.end());
+        v = nearestBigger(row);
+        for (int i = 0; i < v.size(); i++)plane[j][n - 1 - i][1] = v[i];
+        j++;
+    }
+    j = 0;
+    while (j < grid[0].size()) {
+        int n = grid.size();
+        vector<int> temp;
+        for (int i = 0; i < grid.size(); i++) {
+            temp.push_back(grid[i][j]);
+        }
+        auto v = nearestBigger(temp);
+        for (int i = 0; i < grid.size(); i++) {
+            plane[i][j][2] = v[i];
+        }
+        reverse(temp.begin(), temp.end());
+        v = nearestBigger(temp);
+        for (int i = 0; i < grid.size(); i++) {
+            plane[n-1-i][j][3] = v[i];
+        }
+        j++;
+    }
+    int result = 0;
+    for (int i = 0; i < grid.size(); i++) {
+        for (int j = 0; j < grid[0].size(); j++) {
+            int temp = 1;
+            for (int k = 0; k < 4; k++)temp *= plane[i][j][k];
+            result = max(result, temp);
+        }
+    }
+    print(result);
+}
+
+void day8() {
+    //Main idea : we'll find all visible trees by finding all visible trees of height 9, then of height 8, ... , until 1.
+    //We'll maintain a set of all indexes relevant to the calculation, due to the height i.
+    //Let's say we are of height i. Then we'll locate all trees of such height i in our set. 
+    //A tree of height i is visible if and only if there are no trees in our set ahead of him, in all of the 4 directions - in such case,
+    //We'll his index to the set of visible trees.
+
+    ifstream inputFile;
+    string line;
+    vector<vector<int>> grid;
+    set<pair<int, int>> goodTrees;
+
+    inputFile.open("inputFile.txt");
+
+    while (getline(inputFile, line)) {
+        vector<int> row;
+        for (auto ch : line) {
+            row.push_back((int)ch - '0');
+        }
+        grid.push_back(row);
+        row.clear();
+    }
+    inputFile.close();
+
+    vector<vector<pair<int,int>>> trees(10,vector<pair<int,int>>());
+    vector<set<int>> candidatesRows(grid.size(), set<int>());
+    vector<set<int>> candidatesColumns(grid[0].size(), set<int>());
+
+    for (int i = 0; i < grid.size(); i++) {
+        for (int j = 0; j < grid[0].size(); j++) {
+            trees[grid[i][j]].push_back({ i,j });
+        }
+    }
+
+    for (int i = 9; i >= 0; i--) {
+        for (auto tree : trees[i]) {
+            candidatesRows[tree.first].insert(tree.second); //In what row am I?
+            candidatesColumns[tree.second].insert(tree.first);
+        }
+        for (int j = 0; j < grid.size(); j++) {
+            if (candidatesRows[j].size()) {
+                pair<int, int> p = { j,*candidatesRows[j].begin()};
+                goodTrees.insert(p);
+                auto it = candidatesRows[j].end();
+                it--;
+                p = { j,*it };
+                goodTrees.insert(p);
+            }
+        }
+        for (int j = 0; j < grid[0].size(); j++) {
+            if (candidatesColumns[j].size()) {
+                pair<int, int> p = { *candidatesColumns[j].begin(),j };
+                goodTrees.insert(p);
+                auto it = candidatesColumns[j].end();
+                it--;
+                p = { *it,j };
+                goodTrees.insert(p);
+            }
+        }
+    }
+
+    print(goodTrees.size());
+}
+
+void day6V2() {
+    ifstream inputFile;
+    string line;
+    string input;
+
+    inputFile.open("inputFile.txt");
+
+    while (getline(inputFile, line)) {
+        input += line;
+    }
+    inputFile.close();
+
+    map<char, int> window;
+    for (int i = 0; i < 14; i++)window[input[i]]++;
+    if (window.size() == 14) { print(14); return; }
+
+    for (int i = 14; i < input.size(); i++) {
+        if (window[input[i - 14]] == 1)window.erase(input[i - 14]);
+        else {
+            window[input[i - 14]]--;
+        }
+        window[input[i]]++;
+        if (window.size() == 14) { print(i + 1); return; }
+    }
+}
+
+void day6() {
+    ifstream inputFile;
+    string line;
+    string input;
+
+    inputFile.open("inputFile.txt");
+
+    while (getline(inputFile, line)) {
+        input += line;
+    }
+    inputFile.close();
+
+    map<char,int> window;
+    for (int i = 0; i < 4; i++)window[input[i]]++;
+    if (window.size() == 4) { print(4); return; }
+
+    for (int i = 4; i < input.size(); i++) {
+        if (window[input[i - 4]] == 1)window.erase(input[i - 4]);
+        else {
+            window[input[i - 4]]--;
+        }
+        window[input[i]]++;
+        if (window.size() == 4) { print(i+1); return; }
+    }
+}
 
 void day5V2() {
     string input = "";
@@ -288,5 +490,5 @@ void day1() {
 int main()
 {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    day5V2();
+    day8V2();
 }
